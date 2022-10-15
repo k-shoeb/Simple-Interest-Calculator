82ac75b415a5c008b770003a5de15f29be0bbcee
k-shoeb <83496866+k-shoeb@users.noreply.github.com>
2022-10-16 04:49:21 +0530

    Updated script.js
    1. Function to display selected interest Rate.
    2. In compute function values are assigned to variables and condition is given 
       to calculate the interest if the values are positive.
    3. If values are not positive, alert box will appear.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+
+function dispRate() /* function for displaying selected interest rate */
+{
+    window.Rate = document.getElementById("rate").value; /* "window".Rate to make "Rate" a global variable */
+    document.getElementById("rate_val").innerHTML=window.Rate + "%" ;
+}   
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var Principal = document.getElementById("principal").value;
+    var Years = document.getElementById("years").value;
+    var Interest = Principal * Years * window.Rate / 100;
+    var year = new Date().getFullYear()+parseInt(Years);
     
+    if (Principal >= 1) {
+        document.getElementById("result").innerHTML= "<br>" + "If you Deposit, " + Principal + "<br>" +
+        "At a interest rate of \n" + window.Rate + "<br>" +
+        "You will recieve anamount of \n" + Interest + "<br>" + "In the Year " + year ;  
+    }
+    /* Alert box for principal amount <= 0 */
+    else{  
+        alert("Please enter a Positive No.");
+    }
 }
         
