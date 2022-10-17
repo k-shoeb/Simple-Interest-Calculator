da6a0ec54351a2ec1ffcc6f67fc91f107cd92079
k-shoeb <83496866+k-shoeb@users.noreply.github.com>
2022-10-18 00:53:48 +0530

    Final_project.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
-function dispRate() /* function for displaying selected interest rate */
+function updateRate() /* function for displaying selected interest rate */
 {
-    window.Rate = document.getElementById("rate").value; /* "window".Rate to make "Rate" a global variable */
-    document.getElementById("rate_val").innerHTML=window.Rate + "%" ;
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=window.rateVal + "%" ;
 }   
 
 function compute()
 {
     var Principal = document.getElementById("principal").value;
+    var Rate = document.getElementById("rate").value;
     var Years = document.getElementById("years").value;
-    var Interest = Principal * Years * window.Rate / 100;
+    var Interest = Principal * Years * Rate / 100;
     var year = new Date().getFullYear()+parseInt(Years);
     
     if (Principal >= 1) {
-        document.getElementById("result").innerHTML= "<br>" + "If you Deposit, " + Principal + "<br>" +
-        "At a interest rate of \n" + window.Rate + "<br>" +
-        "You will recieve anamount of \n" + Interest + "<br>" + "In the Year " + year ;  
+       document.getElementById("result").innerHTML= "If you Deposit <mark>" + Principal + ",</mark><br>At a interest rate of <mark>" + Rate + 
+        "% ,</mark><br> You will recieve an amount of <mark> " + Interest + "</mark>, <br> In the Year <mark> " + year+ ".</mark><br>";  
     }
     /* Alert box for principal amount <= 0 */
     else{  
